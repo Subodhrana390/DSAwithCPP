0f1f0d9d2f2c7d83a777f9afbc62aaad6ed14207
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-05-18 01:31:41 +0530

    Day 3

--- Array/SwapAlternate.cpp
+++ Array/SwapAlternate.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void swapAlternate(int arr[], int size)
+{
+    for (int i = 0; i < size; i = i + 2)
+    {
+        if (i + 1 < size)
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+}
+
+void PrintArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
+    int odd[5] = {1,
+                  2,
+                  7,
+                  8,
+                  5};
+
+    swapAlternate(odd, 5);
+    cout << endl;
+    PrintArray(odd, 5);
 }
