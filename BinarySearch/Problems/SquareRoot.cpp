7a7bbcb51f4b1c2a2296040bf36328d7e9a9c3c6
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-05-22 23:34:47 +0530

    Binary Search Day 6

--- BinarySearch/Problems/SquareRoot.cpp
+++ BinarySearch/Problems/SquareRoot.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+int SquareRoot(int n)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    long long int ans = -1;
+    while (s <= e)
+    {
+        long long int square = mid * mid;
+        if (square == n)
+        {
+            return mid;
+        }
+        else if (square < n)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
 int main()
 {
 }
