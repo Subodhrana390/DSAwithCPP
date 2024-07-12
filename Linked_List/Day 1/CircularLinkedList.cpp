c2393f9c7f3b4f490e5dfb46f87c0088508b0a3e
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-07-12 10:01:41 +0545

    Day 44 LinkedList -> Circular LL

--- Linked_List/Day 1/CircularLinkedList.cpp
+++ Linked_List/Day 1/CircularLinkedList.cpp
@@ -21,16 +21,83 @@ public:
             this->next = NULL;
         }
     }
+};
+
+void insertNode(Node *&tail, int element, int d)
+{
+    if (tail == NULL)
+    {
+        Node *newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else
+    {
+        // non-empty List
+        Node *curr = tail;
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+        // Out of loop Element found
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
 }
 
-
-
-
-int
-main()
+void deleteNode(Node *&tail, int value)
 {
-    Node *head = NULL;
+    // empty list
+    if (tail == NULL)
+    {
+        cout << "List is Empty" << endl;
+        return;
+    }
+    else
+    {
+        // non-empty list
+        Node *prev = tail;
+        Node *curr = prev->next;
+        while (curr->data != value)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = curr->next;
+
+        if (tail == curr)
+        {
+            tail = prev;
+        }
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void print(Node *&tail)
+{
+    Node *temp = tail;
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+    cout << endl;
+}
+
+int main()
+{
     Node *tail = NULL;
+
+    insertNode(tail, 5, 3);
+    insertNode(tail, 3, 5);
+    insertNode(tail, 5, 7);
+    insertNode(tail, 5, 6);
+    insertNode(tail, 3, 4);
+    print(tail);
+    deleteNode(tail, 7);
+    print(tail);
 
     return 0;
 }
